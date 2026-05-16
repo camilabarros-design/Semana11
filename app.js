81858d0e33b25dc8be7b942cd9f47a60e3e33310
Camila <camilabarros.puc@gmail.com>
2026-05-16 12:34:28 -0300

    Adicionando o JS

--- app.js
+++ app.js
@@ -1,1 +1,189 @@
 
+const dados = [
+    {
+        "id": 1,
+        "titulo": "Asas reluzentes",
+        "descricao": "Descubra as origens do romance entre a rainha do Refúgio das Fadas e o senhor do Bosque do Inverno.",
+        "conteudo": "Prestes a ser coroada no Refúgio das Fadas, a jovem Clarion é fascinada pelo proibido Bosque do Inverno. Quando surge o boato de que um monstro congelado ameaça a Primavera, ela vê a chance perfeita de provar seu valor e decide investigar a fronteira.Em vez de uma criatura, Clarion encontra Milori, um jovem guardião do inverno. Juntos, eles se aliam para salvar seus reinos, mas a forte conexão que surge entre os dois revela o verdadeiro e mortal motivo pelo qual as fadas das estações quentes e frias nunca devem se misturar.",
+        "categoria": "Fantasia",
+        "autor": "Allison Saft ",
+        "data": "2025",
+        "imagem": "https://m.media-amazon.com/images/I/41vuuhPjTeL._SY445_SX342_ML2_.jpg"
+    },
+    {
+        "id": 2,
+        "titulo": "A maldição de prata",
+        "descricao": "Nesta fantasia contemporânea inspirada na lenda do Rei Arthur, Tamsin Lark precisa correr contra o tempo para quebrar uma maldição e salvar quem ama. Primeiro volume da série de Alexandra Bracken, o livro é um best-seller do New York Times repleto de mistério, romance e reviravoltas.",
+        "conteudo": "Sem magia, Tamsin Lark e seu irmão sobrevivem como esvaziadores no submundo mágico de Boston desde o sumiço do pai adotivo. Sete anos depois, uma poderosa feiticeira lança uma competição mortal pelo anel da lenda do Rei Arthur — a última relíquia que o pai dos jovens teria encontrado antes de desaparecer.Para vencer a caçada implacável e proteger quem ama, Tamsin é forçada a se aliar ao seu charmoso arqui-inimigo, o mágico Emrys Dye. Juntos, eles embarcam em uma jornada rumo às ruínas de Avalon, enfrentando magia sombria, segredos letais e uma paixão inesperada.",
+        "categoria": "Fantasia",
+        "autor": "Alexandra Bracken",
+        "data": "2024",
+        "imagem": "https://m.media-amazon.com/images/I/81pNKCu1SkL._SY385_.jpg"
+    },
+    {
+        "id": 3,
+        "titulo": "O beijo da neve",
+        "descricao": "Nina e Elyan são patinadores mas as semelhanças terminam por aí . Ela irradia alegria, enquanto ele tem um coração de gelo... Mas a química entre os dois é capaz de derreter tudo pelo caminho.",
+        "conteudo": "Após um afastamento forçado, a jovem patinadora Nina tenta retomar sua carreira dividindo-se entre treinos intensos e o trabalho. Sua rotina muda completamente com a chegada de Elyan Kane, um frio e intenso patinador olímpico que passou cinco anos sumido após a morte trágica de sua antiga parceira.Ele surge com uma proposta irrecusável, sem imaginar que Nina nutria por ele uma paixão platônica na adolescência que terminou em desilusão. Para perseguir seus sonhos, ela aceita o desafio de conviver com o homem que balança seu coração.Entre pistas de gelo no Canadá, auroras boreais e um castelo supostamente mal-assombrado, o casal enfrentará traumas, medos e sentimentos contraditórios. O livro O beijo da neve, de Babi A. Sette, constrói um envolvente romance de inimigos para amantes que mistura a magia da patinação artística com uma jornada emocionante e arrebatadora.",
+        "categoria": "Romance",
+        "autor": "Babi A. Sette",
+        "data": "2023",
+        "imagem": "https://m.media-amazon.com/images/I/51p2R9ZAwOL._SY445_SX342_ML2_.jpg"
+    },
+    {
+        "id": 4,
+        "titulo": "A Seleção",
+        "descricao": "Muitas garotas sonham em ser princesas, mas este não é o caso de America Singer. Ela topa se inscrever na Seleção só para agradar a mãe, certa de que não será sorteada para participar da competição em que o príncipe escolherá sua futura esposa. Mas é claro que depois disso sua vida nunca mais será a mesma...",
+        "conteudo": "Para trinta e cinco garotas, a Seleção é a chance de uma vida. É a oportunidade de ser alçada a um mundo de vestidos deslumbrantes e joias valiosas. De morar em um palácio, conquistar o coração do belo príncipe Maxon e um dia ser a rainha.America Singer, no entanto, estar entre as Selecionadas é um pesadelo. Significa deixar para trás o rapaz que ama. Abandonar sua família e seu lar para entrar em uma disputa ferrenha por uma coroa que ela não quer. E viver em um palácio sob a ameaça constante de ataques rebeldes.Então America conhece pessoalmente o príncipe - e percebe que a vida com que sempre sonhou talvez não seja nada comparada ao futuro que nunca tinha ousado imaginar.",
+        "categoria": "Romance",
+        "autor": "Kiera Cass",
+        "data": "2012",
+        "imagem": "https://m.media-amazon.com/images/I/81ql6xkkliL._SY425_.jpg"
+    },
+    {
+        "id": 5,
+        "titulo": "Verity",
+        "descricao": "Um casal apaixonado. Uma intrusa. Três mentes doentias. Finalista do prêmio Goodreads como melhor romance de 2019, Verity é o primeiro thriller de Colleen Hoover e deixa os leitores envolvidos do começo ao fim.",
+        "conteudo": "A autora best-seller Verity Crawford fica incapacitada após um grave acidente, deixando sua famosa série de livros inacabada. Para concluir a obra, o marido de Verity, Jeremy, contrata Lowen Ashleigh, uma escritora à beira da falência.Ao se mudar temporariamente para a casa da família para pesquisar as anotações do projeto, Lowen encontra uma autobiografia oculta de Verity. O manuscrito traz relatos perturbadores sobre o passado do casal e as tragédias que envolveram suas filhas.Conforme se apaixona por Jeremy, Lowen se vê presa em um perigoso jogo psicológico e precisa decidir se revela a verdade sombria sobre a esposa dele ou se mantém o segredo. Afastando-se dos romances tradicionais, Colleen Hoover entrega em Verity um suspense psicológico chocante sobre os limites das relações humanas.",
+        "categoria": "Suspense",
+        "autor": "Colleen Hoover",
+        "data": "2020",
+        "imagem": "https://m.media-amazon.com/images/I/51ABRx3d7GL._SY445_SX342_ML2_.jpg"
+    },
+    {
+        "id": 6,
+        "titulo": "Ninguém nunca vai saber",
+        "descricao": "Uma narrativa acelerada e que manterá o leitor preso até a última página enquanto Eve procura por respostas e luta para proteger seu filho.",
+        "conteudo": "Grávida, falida e sem família após perder o namorado em um acidente, Eve Sylvester aceita uma proposta tentadora: ser babá para um casal rico em uma ilha isolada na costa da Tasmânia. A única exigência de Julia e Cristopher Hygate é o sigilo absoluto sobre o emprego.Isolada do mundo, Eve começa a desconfiar dos motivos que levaram o casal a contratar uma grávida sem experiência. Conforme se envolve na rotina deles, ela percebe que os Hygate escondem intenções sinistras e que, se algo acontecer com ela naquele lugar esquecido, ninguém jamais saberá.",
+        "categoria": "Suspense",
+        "autor": "Rose Carlyle",
+        "data": "2025",
+        "imagem": "https://m.media-amazon.com/images/I/91Wp-cx+20L._SY385_.jpg"
+    },
+    {
+        "id": 7,
+        "titulo": "O vilarejo",
+        "descricao": "lustrações coloridas dão vida a romance com elementos de horror gótico e suspense.Do autor da novela Beleza Fatal, da Max e criador da série original Netflix Bom dia, Verônica.",
+        "conteudo": "Em 1589, o padre e demonologista Peter Binsfeld fez a ligação de cada um dos pecados capitais a um demônio, supostamente responsável por invocar o mal nas pessoas. É a partir daí que Raphael Montes cria sete histórias situadas em um vilarejo isolado, apresentando a lenta degradação dos moradores do lugar, e pouco a pouco o próprio vilarejo vai sendo dizimado, maculado pela neve e pela fome.As histórias podem ser lidas em qualquer ordem, sem prejuízo de sua compreensão, mas se relacionam de maneira complexa, de modo que ao término da leitura as narrativas convergem para uma única e surpreendente conclusão.",
+        "categoria": "Terror",
+        "autor": "Raphael Montes",
+        "data": "2015",
+        "imagem": "https://m.media-amazon.com/images/I/81JaIN9NExL._SY385_.jpg"
+    },
+    {
+        "id": 8,
+        "titulo": "A cirurgiã",
+        "descricao": "Cirurgiã renomada, esposa amorosa e... assassina? Antes de seu mundo desabar, a doutora Anne Wiley tinha uma vida perfeita",
+        "conteudo": "A carreira de sucesso que sempre almejou conquistar, uma bela casa onde poderia descansar confortavelmente após um longo dia de trabalho e um marido extremamente dedicado e cuidadoso, cujo sorriso sempre a manteve segura. Até aquele momento, ela nunca tinha perdido um paciente. Mas também nunca havia operado alguém que odiava. No minuto seguinte em que declarou o óbito, toda a equipe médica na sala de operações a observou em silêncio, confusos e preocupados. O olhar de Anne transmitia medo, o coração batia acelerado e as mãos tremiam e suavam dentro das luvas. Seus doze anos de experiência são colocados sob suspeita quando é descoberto quem era o paciente que faleceu em suas mãos. Empenhada em acusar a doutora de assassinato, a promotora criminal Paula Fuselier começa a investigar. No entanto, tanto Anne quanto a promotora têm algo muito particular em comum – um relacionamento com Derreck, marido de Anne, que é candidato a prefeito, e qualquer escândalo pode prejudicar sua carreira na vida pública. Um suspense psicológico totalmente envolvente que fará você correr pelas páginas até o final.",
+        "categoria": "Terror",
+        "autor": "Leslie Wolfe",
+        "data": "2024",
+        "imagem": "https://m.media-amazon.com/images/I/71LBtlVGqHL._SY425_.jpg"
+    },
+    {
+        "id": 9,
+        "titulo": "Dessa vez é real",
+        "descricao": "Quando todo mundo está assistindo, será possível encontrar o amor de verdade?",
+        "conteudo": "Eliza Lin, de dezessete anos, acabou de chegar em Pequim e sente falta de sua melhor amiga e da vida que deixou nos Estados Unidos. Agora, ela não só tem que se adaptar a uma nova realidade, mas também lidar com mais uma reviravolta: a redação que ela fez sobre ter encontrado sua alma gêmea na cidade viraliza da noite pro dia.De repente, ela tem a aprovação de seus colegas, uma proposta de estágio em uma grande revista e um segredo gigantesco: Eliza inventou a redação inteira! Ela nunca sequer se apaixonou, que dirá ter um relacionamento. Desesperada para esconder a verdade, a garota faz um acordo com um ator famoso de sua turma, o charmoso e indiferente Caz Song. Nada como um namoro falso para despistar os curiosos.Mas quando o relacionamento com Caz se torna um pouco convincente demais, todo o plano de Eliza é colocado em risco. Será que ela vai conseguir seguir adiante mesmo se isso significar partir o próprio coração?",
+        "categoria": "Dorama",
+        "autor": "Ann Liang",
+        "data": "2023",
+        "imagem": "https://m.media-amazon.com/images/I/81mMVjmCZAL._SY425_.jpg"
+    },{
+        "id": 10,
+        "titulo": "O horizonte mora em um dia cinza",
+        "descricao": "E se a superação da dor levar mais tempo que o esperado? Afinal, cada um tem seu próprio tempo de prantear.",
+        "conteudo": "Ayla Vasconcellos está em meio ao processo de esperar que as feridas se tornem cicatrizes quando, inesperadamente, ocorre o esbarrão. Os óculos voando outra vez. Seria apenas coincidência o fato de Joon Hyuk, aquele coreano de olhos angulares e voz melodiosa, estar ali à sua frente, naquele exato segundo, numa universidade na Coreia do Sul? Ou algo mais profundo aguardava aqueles dois jovens que pertenciam a culturas tão distintas e que, no entanto, pareciam dividir as mesmas dores e as mesmas esperanças?",
+        "categoria": "Dorama",
+        "autor": "Tatielle Katluryn",
+        "data": "2024",
+        "imagem": "https://m.media-amazon.com/images/I/91gtU+6jHjL._SY425_.jpg"
+    },
+    {
+        "id": 11,
+        "titulo": "A coragem de não agradar",
+        "descricao": "Na periferia de uma cidade milenar vivia um filósofo que ensinava que o mundo era simples e que a felicidade estava ao alcance de todos. Certo dia, um jovem insatisfeito com a vida foi desafiá-lo a provar sua tese.",
+        "conteudo": "Com mais de 3 milhões de exemplares vendidos, A coragem de não agradar conta uma história capaz de iluminar nosso poder interior e nos permitir ser quem somos.Inspirado nas ideias de Alfred Adler – um dos expoentes da psicologia ao lado de Sigmund Freud e Carl Jung –, o livro apresenta o debate transformador entre um jovem e um filósofo.Ao longo de cinco noites, eles discutem temas como autoestima, raiva, autoaceitação e complexo de inferioridade. Aos poucos, fica claro que libertar-se das expectativas alheias e das dúvidas que nos paralisam e encontrar a coragem para mudar está ao alcance de todos.Assim como nos diálogos de Platão, em que o conhecimento vai sendo construído através do debate, o filósofo oferece ao rapaz as ferramentas necessárias para que ele se torne capaz de se -reinventar e de dizer não às limitações impostas por si mesmo e pelos outros.",
+        "categoria": "Autoajuda",
+        "autor": "Ichiro Kishimi e Fumitake Koga ",
+        "data": "2018",
+        "imagem": "https://m.media-amazon.com/images/I/51TgheB19FL._SY425_.jpg"
+    },
+    {
+        "id": 12,
+        "titulo": "O poder do subconsciente",
+        "descricao": "Aprenda a usar o subconsciente a seu favor e alcance a felicidade e a prosperidade que você merece.",
+        "conteudo": "Encontrar o caminho para o sucesso em todas as áreas da vida é mais fácil do que imaginamos: basta nos voltarmos para nosso interior. O segredo para tomarmos o rumo de uma vida plena, próspera e feliz está em nosso subconsciente, e é ele que dispõe de todos os recursos para alcançarmos qualquer objetivo.Mas como podemos acessar esses recursos e tirar proveito de todas as suas potencialidades?O aclamado escritor Dr. Joseph Murphy mostra como é simples nos conectarmos com a sabedoria infinita do subconsciente e utilizá-la para realizarmos grandes transformações em nossa trajetória. De maneira descomplicada e objetiva, ele fornece diversas técnicas e exercícios para explorarmos essa mina de ouro.",
+        "categoria": "Autoajuda",
+        "autor": "Joseph Murphy ",
+        "data": "2023",
+        "imagem": "https://m.media-amazon.com/images/I/91h1osfHs+L._SY425_.jpg"
+    },
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.getElementById("container-livros")) {
+        renderizarHome();
+    } else if (document.getElementById("container-detalhes")) {
+        renderizarDetalhes();
+    }
+});
+
+function renderizarHome() {
+    const container = document.getElementById("container-livros");
+    container.innerHTML = "";
+
+    dados.forEach(item => {
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+            <a href="detalhes.html?id=${item.id}">
+                <img src="${item.imagem}" alt="${item.titulo}" class="card-img">
+            </a>
+            <div class="card-corpo">
+                <span class="tag-categoria">${item.categoria}</span>
+                <h2><a href="detalhes.html?id=${item.id}">${item.titulo}</a></h2>
+                <p class="autor-card">Por: ${item.autor}</p>
+                <p>${item.descricao}</p>
+            </div>
+        `;
+        container.appendChild(card);
+    });
+}
+
+function renderizarDetalhes() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const idLivro = parseInt(urlParams.get("id"));
+
+    const livro = dados.find(item => item.id === idLivro);
+    const container = document.getElementById("container-detalhes");
+
+    if (!livro) {
+        container.innerHTML = `
+            <div class="erro-noticia">
+                <h2>Livro não encontrado no catálogo!</h2>
+                <a href="index.html" class="btn-voltar">Voltar ao Catálogo</a>
+            </div>
+        `;
+        return;
+    }
+
+    container.innerHTML = `
+        <a href="index.html" class="btn-voltar">← Voltar para o Catálogo</a>
+        <article class="conteudo-artigo">
+            <span class="tag-categoria">${livro.categoria}</span>
+            <h1>${livro.titulo}</h1>
+            <div class="metadados">
+                Autor: <strong>${livro.autor}</strong> | Ano de Publicação: <strong>${livro.data}</strong>
+            </div>
+            <div class="layout-detalhe-livro">
+                <img src="${livro.imagem}" alt="${livro.titulo}" class="img-livro-detalhe">
+                <div class="texto-noticia">
+                    <h3>Sinopse Completa:</h3>
+                    <p>${livro.conteudo}</p>
+                </div>
+            </div>
+        </article>
+    `;
+}
